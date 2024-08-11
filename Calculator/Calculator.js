3b5731bfc48c8880e782ffd7f40a07e810a3715f
SUBRAT KHATUA <subratkhatua462@gmail.com>
2024-08-11 20:14:16 +0530

    Task Complete

---
+++ Calculator/Calculator.js
@@ -0,0 +1,11 @@
+function calcNumbers(result){
+    form.displayResult.value=form.displayResult.value+result;
+    function clearScreen() {
+        document.getElementById("result").value = "";
+    }
+    
+}
+function Clear() {
+    var inp = document.getElementById('res');
+    inp.value = '';
+ }
